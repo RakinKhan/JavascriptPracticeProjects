e6a3ee13214adabc3dc6b85a176119c7a1b62ceb
RakinKhan <47644682+RakinKhan@users.noreply.github.com>
2019-04-12 15:25:30 -0400

    Update expensecalcupdate.js

--- JavaScript/expensecalcupdate.js
+++ JavaScript/expensecalcupdate.js
@@ -1,36 +1,43 @@
 let account = {
     name: 'Rakin Khan',
-    expenses: []
+    expenses: [],
+    incomes: []
 }
-
+ 
 let addExpense = function (description, amount) {
     let expense = {
         description: description,
         amount: amount
     }
     return account.expenses.push(expense)
-
 }
 
+let addIncome = function (description, amount) {
+    let income = {
+        description: description,
+        amount: amount
+    }  
+    return account.incomes.push(income)
+}
+ 
 let getAccountSummary = function () {
     let totalExpense = 0
-
+    let totalIncome = 0
+ 
     account.expenses.forEach(function (expense) {
         totalExpense = totalExpense + expense.amount
     })
-
-    return `${account.name} has $${totalExpense}`
-
+    
+    account.incomes.forEach(function (income) {
+        totalIncome = totalIncome + income.amount
+    })
+ 
+    return `${account.name} has a balance of $${totalIncome - totalExpense}, $${totalIncome} in income, $${totalExpense} in expenses.`
 }
-
-
-
-
-
-
-
+ 
 addExpense('Rent', 950)
 addExpense('Coffee', 2)
-
+addIncome('Job', 1000)
+ 
 console.log(account)
 console.log(getAccountSummary(account))
