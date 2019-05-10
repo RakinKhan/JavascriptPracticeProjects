2949ecd2371be6344ee110436ccf2b0e2df02212
RakinKhan <47644682+RakinKhan@users.noreply.github.com>
2019-05-09 21:38:22 -0400

    Update grade-calc.js

--- JavaScript/grade-calc.js
+++ JavaScript/grade-calc.js
@@ -1,23 +1,32 @@
 let gradeCalc = function (studentScore, maxScore) {
-    let numberGrade = (studentScore / maxScore) * 100
-    let letterGrade = ' '  
-    if (numberGrade >= 90) {
-        letterGrade = 'A'
-        return `You got a ${letterGrade} (${numberGrade})!`
-    }   else if (numberGrade >= 80) {
-        letterGrade = 'B'
-        return `You got a ${letterGrade} (${numberGrade})!`       
-    }   else if (numberGrade >= 70) {
-        letterGrade = 'C'
-        return `You got a ${letterGrade} (${numberGrade})!`       
-    }   else if (numberGrade >= 60) {
-        letterGrade = 'D'
-        return `You got a ${letterGrade} (${numberGrade})!`       
-    }   else if (numberGrade <= 59) {
-        letterGrade = 'F'   
-        return `You got a ${letterGrade} (${numberGrade})!` 
+    if (typeof studentScore === 'number' && typeof maxScore === 'number) {
+        let numberGrade = (studentScore / maxScore) * 100
+        let letterGrade = ' '  
+        if (numberGrade >= 90) {
+            letterGrade = 'A'
+            return `You got a ${letterGrade} (${numberGrade})!`
+        }   else if (numberGrade >= 80) {
+            letterGrade = 'B'
+            return `You got a ${letterGrade} (${numberGrade})!`       
+        }   else if (numberGrade >= 70) {
+            letterGrade = 'C'
+            return `You got a ${letterGrade} (${numberGrade})!`       
+        }   else if (numberGrade >= 60) {
+            letterGrade = 'D'
+            return `You got a ${letterGrade} (${numberGrade})!`       
+        }   else if (numberGrade <= 59) {
+            letterGrade = 'F'   
+            return `You got a ${letterGrade} (${numberGrade})!` 
+        }
+    } else {
+        throw Error('Please Enter A Number')
     }
 }
 
-let grade = gradeCalc(19, 20)
-console.log(grade)
+try {
+    let grade = gradeCalc(19, 20)
+    console.log(grade)    
+} else (e) {
+    console.log(e.message)
+}
+
